12af83a8b45463a72c31bd33fc68e6e3d24516b3
kumataahh <153236205@qq.com>
2020-11-13 13:37:05 +0800

    update

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "fmt"
+  "flag"
+  "fmt"
 )
 
+var name string
 
+func init() {
+  
+}
 
 func main() {
-
-    s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
-    s4 := s3[3:6]
-    fmt.Printf("The length of s4: %d\n", len(s4))
-    fmt.Printf("The capacity of s4: %d\n", cap(s4))
-    fmt.Printf("The value of s4: %d\n", s4)
-
+  // 需在此处添加代码。[3]
+  fmt.Printf("Hello, %s!\n", name)
 }
