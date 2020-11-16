17e147a687f888a8e5808ed10e29efb9f7c54b12
kumataahh <153236205@qq.com>
2020-11-16 19:29:08 +0800

    update

--- goku.go
+++ goku.go
@@ -1,40 +1,11 @@
+
 package main
 
-import (
-  "flag"
-  "fmt"
-)
+import "fmt"
 
+var container = []string{"zero", "one", "two"}
 
-
-type Config struct{
-    name string
-    age string
-    bornDate string
-    interest string
-}
-
-
-// Define INFO in here or Next Step
-var cfg Config = Config{
-    name: "kawavi",
-    age: "20",
-    bornDate: "20000101",
-    interest: "game",
-}
-
-
-
-func init(){
-    flag.StringVar(&cfg.name,     "n", "kawayi", "Name input.")
-    flag.StringVar(&cfg.age,      "a", "20", "Age input.")
-    flag.StringVar(&cfg.bornDate, "b", "20200101", "bornDate input.")
-    flag.StringVar(&cfg.interest, "i", "game", "interest input.")
-}
-
-
-func main(){
-    // TODO parse from command
-    flag.Parse()
-    fmt.Printf("Name: %s\nAge: %s\nDate: %s\nInterest: %s\n", cfg.name, cfg.age, cfg.bornDate, cfg.interest)
+func main() {
+  container := map[int]string{0: "zero", 1: "one", 2: "two"}
+  fmt.Printf("The element is %q.\n", container[0])
 }
