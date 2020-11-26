0d187bcfa2fd20d59c85db21b54cee6dd3edbd9b
kumataahh <153236205@qq.com>
2020-11-26 18:05:05 +0800

    update

--- goku.go
+++ goku.go
@@ -2,16 +2,32 @@
 package main
 
 import(
-    // "fmt"
-    . "gotools/practice"
+    "fmt"
+    "errors"
+    // . "goku/basics"
 )
 
 
 
-func main() {
-    MapCreate()
+func EchoEmpty(request string)(response string, err error){
+    if request == ""{
+        err = errors.New("Can not empty!")
+        return
+    }
+    response = fmt.Sprintf("Echo: %s\n", request)
+    return
 }
 
 
 
-
+func main() {
+    for _, req := range []string{"", "hello!"} {
+      fmt.Printf("request: %s\n", req)
+      resp, err := Echo(req)
+      if err != nil {
+        fmt.Printf("error: %s\n", err)
+        continue
+      }
+      fmt.Printf("response: %s\n", resp)
+    }
+}
