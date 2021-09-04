7cf9b16260bff438f7940f7e09ac9d0fe077ef2b
kumata <kumata@qq.com>
2021-09-04 23:06:34 +0800

    update algo and add quick_sort

--- cmd/goku/main.go
+++ cmd/goku/main.go
@@ -12,7 +12,7 @@ func main(){
     arr := []int{10, 4, 41, 12, 32, 5, 22, 1, 6, 11}
     fmt.Println(arr)
 
-    result := algo.BubbleSortOpt1(arr)
+    result := algo.QuickSort(arr, 0, len(arr)-1)
     fmt.Printf("result: %d\n", result)
 
 }
--- internal/algo/sort_quick.go
+++ internal/algo/sort_quick.go
@@ -4,6 +4,45 @@ import(
 
 )
 
-func QuickSort(arr []int) []int {
+
+/* 
+快速排序，时间复杂度 O(nlogn)
+分两个函数，一个找中心轴函数，一个是递归快排函数
+*/
+
+// 1. 快排
+// 递归
+func QuickSort(arr []int, left, right int) []int {
+	if left < right{
+		pivot := partition(arr, left, right)
+		QuickSort(arr, left, pivot - 1)
+		QuickSort(arr, pivot + 1, right)
+	}
+
 	return arr
 }
+
+// 2. 选取一个数作为中心轴，划分左右分区alice `low` and `high`
+// 对数据元素进行左右调整，小于中心值移到左边，大于则移到右边
+func partition(arr []int, low, high int) int {
+	pivot := arr[low]
+	for low < high{
+		
+		for low < high && arr[high] > pivot{
+			high --
+		}
+		arr[low] = arr[high]
+
+		for low < high && arr[low] < pivot{
+			low ++
+		}
+		arr[high] = arr[low]
+	}
+	// after loop, low, high 重合
+	arr[low] = pivot
+	return low
+}
+
+
+
+
